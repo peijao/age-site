2de09d4e86b130855977a82558e86743e27f2664
Gor <93428900+peijao@users.noreply.github.com>
2025-06-08 14:39:44 +0300

    Update ContactModal.jsx

--- src/components/ContactModal.jsx
+++ src/components/ContactModal.jsx
@@ -25,7 +25,7 @@ const ContactModal = ({ onClose }) => {
     }
 
     try {
-      const response = await fetch("http://localhost:3001/send", {
+      const response = await fetch("https://ageinvest-backend.onrender.com/send", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
