c31427cb4d5a9a1136a6b5b0f5f9337bd2ccfd85
Serhii <serhii.arkhipov@openexc.com>
2025-09-04 22:49:44 +0300

    Fixed config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import javascriptObfuscator from 'rollup-plugin-javascript-obfuscator';
 
 export default defineConfig({
-  base: '/',
+  base: '/age-site/',
   plugins: [
     react(),
     {
